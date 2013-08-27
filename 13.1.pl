674e96bf4113f70c7158c9c76508d5e508082dd9
syedaali <syedali@oceanwear-lm.(none)>
2013-08-26 21:29:40 -0700

    13.1

---
+++ 13.1.pl
@@ -0,0 +1,9 @@
+#count files in a directory, similar to 'ls -a -1 | wc -l' without the . and ..
+print qq{ enter directory to count files in ->  };
+my $dir=<STDIN>;
+chomp ($dir);
+opendir DIR,$dir or die "Could not open dir $dir! $!";
+my @files=readdir(DIR);
+my $count=@files-2;
+print qq{your directory $dir has $count files \n};
+closedir (DIR);
